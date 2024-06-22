7812e804d3c3a8858bde970102feec1d029cd35a
Sudhanva Samaga <sudhanvavsamaga@gmail.com>
2024-06-22 15:36:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const express = require('express');
 const nodemailer = require('nodemailer');
 const path = require('path');
-
 const fs = require('fs');
 const multer = require('multer');
 const { jsPDF } = require('jspdf');
